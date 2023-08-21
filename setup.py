5058632bbca6e60342f47eab765dde6890a84a8e
John Brown <john.brown@gearboxsoftware.com>
2023-08-21 13:47:25 -0500

    update to 11.307.0

--- setup.py
+++ setup.py
@@ -67,18 +67,17 @@ setup(
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     install_requires=[
-        "docker-compose == 1.29.2",
-        "semver == 3.0.0",
-        "setuptools == 67.8.0",
-        "wheel == 0.40.0",
+        "semver == 3.0.1",
+        "setuptools == 68.1.2",
+        "wheel == 0.41.1",
     ],
     extras_require={
         "test": [
             "coverage == 6.5.0",
             "coveralls == 3.3.1",
-            "docker == 6.1.2",
-            "pre-commit == 3.3.2",
-            "pytest == 7.3.1",
+            "docker == 6.1.3",
+            "pre-commit == 3.3.3",
+            "pytest == 7.4.0",
             "pytest-cov == 4.1.0",
             "pytest-lazy-fixture == 0.6.3",
         ]
