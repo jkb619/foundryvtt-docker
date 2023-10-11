b72495a313809d1366e47277c1b3b7ff96852503
John Brown <john.brown@gearboxsoftware.com>
2023-10-11 09:57:51 -0500

    11.313.0

--- tulta-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SSH_PUB_KEY="$(cat ~/.ssh/id_rsa.pub)"
-echo "$SSH_PUB_KEY"
-# old 10.291.0
-docker build \
-  --build-arg VERSION=11.308.0 \
-  --build-arg SSH_PUB_KEY="$SSH_PUB_KEY" \
-  --tag jobrown/foundryvtt:11.308.0 .
