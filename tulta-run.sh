dcb7e49d160e55a61bd7af217cc4c33248e7006e
John Brown <john.brown@gearboxsoftware.com>
2023-10-11 09:59:45 -0500

    11.313.0

--- tulta-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run  -it --env FOUNDRY_USERNAME='tulta-munille'   --env FOUNDRY_PASSWORD='Wp1u8Y4Efgs4'   --publish 30000:30000/tcp --publish 22:22/tcp --volume /home/john/foundrydata:/data   jobrown/foundryvtt:11.308.0 
